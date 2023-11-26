72e8e464fe57d6ec8d085c5ee9f68564cf8206a9
Scott_Squires <scottsquires567@gmail.com>
2023-11-26 11:28:24 -0800

    ADD: Players in prototype scene

---
+++ UVEC/Assets/Prototype/TestPlayerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class TestPlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _rotateSpeed = 50f;
+    [SerializeField] private float _lookUpDownSpeed = 20f;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float camUpMax = 130f;
+    [SerializeField] private float camDownMax = 70f;
+    
+    
+    
+    
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * _speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * Time.deltaTime * _speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * _speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * Time.deltaTime * _speed;
+        }
+        
+        transform.Rotate(0f, Input.GetAxis("Mouse X") * _rotateSpeed * Time.deltaTime, 0f);
+        _camera.transform.Rotate(-Input.GetAxis("Mouse Y") * _lookUpDownSpeed * Time.deltaTime, 0f, 0f);
+    }
+}
